6d4d4b550597ceb4429a905b7989167d28184a1b
Chophel98 <chophel9808@gmail.com>
2020-10-17 14:57:10 +0600

    Final Commit

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,6 +1,6 @@
 class PasswordsController < ApplicationController
 
-    before_action :authorized, except: [:forgot, :reset, :update]
+    before_action :authorized, only: [:update], except: [:forgot, :reset]
 
     def forgot
         if params[:email].blank?
@@ -16,7 +16,7 @@ class PasswordsController < ApplicationController
         subject = "Token"
         body = "This is your TOKEN to reset your password #{token} use it before it expires"
         ApplicationMailer.send_email(user, subject, body).deliver_now
-            render json: { success: true, message: "OTP has sent to your registered email" }, status: 200
+            render json: { success: true, message: "OTP has been sent to your registered email" }, status: 200
         else
             render json: { success: false, error: "Email address not found. Please check and try again." }, status: 404
         end
@@ -41,16 +41,22 @@ class PasswordsController < ApplicationController
         end
     end
 
+
     def update
-        if !params[:password].present?
-            render json: {error: 'Password not present'}, status: :unprocessable_entity
-            return
-        end
-      
-        if current_user.reset_password(params[:password])
-            render json: {status: 'ok'}, status: :ok
+        user = User.find_by(email: params[:email])
+        if user
+            if !params[:password].present?
+                render json: {error: 'No matching password found'}, status: :unprocessable_entity
+                return
+            end
+
+            if user.reset_password!(params[:password])
+                render json: {status: 'Password updated successfully'}, status: :ok
+            else
+                render json: {errors: user.errors.full_messages}, status: :unprocessable_entity
+            end
         else
-            render json: {errors: current_user.errors.full_messages}, status: :unprocessable_entity
+            render json: { error: 'Email does not match'}, status: 400
         end
     end
 end
